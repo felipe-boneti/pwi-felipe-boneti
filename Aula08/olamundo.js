3ed29b7c0b55879af5439067f9ed8d0f4c279bad
felipe-boneti <felipe_gustavo_boneti@outlook.com>
2022-10-26 22:22:56 -0300

    Aula 8 - JavasScript

---
+++ Aula08/olamundo.js
@@ -0,0 +1,18 @@
+console.log("Olá mundo!");
+/*console.lo é um comando que vai exibir 
+informações no console do navegador.*/
+
+var Variavel1 = 2;
+//var tem o tipo escopo global
+let Variavel2 = "Olá";
+//let tem o tipo de escopo local
+const Variavel3 = 3 + 3;
+//const é de escopo global e não pode ser reescrito
+
+console.log ("A variavel Variavel1 é igual a ", Variavel1)
+Variavel1 = 4;
+console.log ("Agora variavel1 é igual a ", Variavel1)
+console.log ("O valor de Variavel 2 é igual a ", Variavel2)
+Variavel2 = "Agora vai "
+console.log ("Agora o valor da Variavel2 é ", Variavel2)
+console.log ("Essa é a constante ", Variavel3)
